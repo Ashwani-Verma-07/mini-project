e41526d13343169c7869a2dfb68e470b8164e54e
Ashwani Verma <89683890+Ashwani-Verma-07@users.noreply.github.com>
2021-12-24 22:27:34 +0530

    Added counter, modified styles and make responsive

--- script.js
+++ script.js
@@ -6,3 +6,22 @@ open.addEventListener('click', () => container.classList.add('show-nav'))
 
 close.addEventListener('click', () => container.classList.remove('show-nav'))
 
+var counters = document.querySelectorAll('.counter')
+counters.forEach(counter => {
+    
+    let count=1;
+    const updateCounter = () => {
+        const target = counter.getAttribute('data-target')
+
+            setInterval(()=>{
+                if(count<target){
+                count++;
+                counter.innerText=count;
+                }
+            },)
+            //setTimeout(updateCounter, 50)
+        
+    }
+    updateCounter()
+})
+
